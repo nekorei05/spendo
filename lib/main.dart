fdab90f48b6d1cdf08e3adb0f53ee1fe9f4779e3
Richa <reikhandekar05@gmail.com>
2025-09-30 21:16:46 +0530

    👤 provided session management using FirebaseAuth to persist login state

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,65 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
+
 import 'screens/onboarding_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
+import 'services/theme_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(
+    ChangeNotifierProvider(create: (_) => ThemeProvider(), child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return MaterialApp(
       title: 'Expense Tracker',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      initialRoute: '/',
-      routes: {
-        '/': (_) => OnboardingScreen(),
-        '/login': (_) => LoginScreen(),
-        '/home': (_) => HomeScreen(),
+      theme: ThemeData.light().copyWith(
+        primaryColor: Color(0xFF7345EE),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Color(0xFF7345EE),
+        ),
+      ),
+      darkTheme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF7345EE),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Color(0xFF7345EE),
+        ),
+      ),
+      themeMode: themeProvider.mode,
+      home: AuthGate(), // 👈 This decides which screen to show
+    );
+  }
+}
+
+// 👇 This widget decides if user goes to Home or Login
+class AuthGate extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // While checking the auth state
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(body: Center(child: CircularProgressIndicator()));
+        }
+
+        // If user is logged in
+        if (snapshot.hasData) {
+          return HomeScreen();
+        }
+
+        // If user is NOT logged in
+        return OnboardingScreen(); // or LoginScreen if you prefer
       },
     );
   }
